4c64f0d1bd377d5ebf9fe12cd37d4c9694458079
Jone Wang <i@jone.wang>
2022-10-17 12:39:49 +0800

    Update.

--- Sources/InkMoya/PluginType.swift
+++ Sources/InkMoya/PluginType.swift
@@ -6,6 +6,9 @@
 //
 
 import Foundation
+#if canImport(FoundationNetworking)
+    import FoundationNetworking
+#endif
 
 public protocol PluginType {
     func prepare(_ request: URLRequest, target: TargetType) -> URLRequest
